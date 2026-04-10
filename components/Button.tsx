43fc87b2f0306b098d12ff068300764e1ff5e8b2
Askebir Tadesse <askebirtadesse@gmail.com>
2026-04-10 21:47:44 +0300

    get App featurer

--- components/Button.tsx
+++ components/Button.tsx
@@ -6,16 +6,19 @@ type ButtonProps = {
   title: string;
   icon?: string;
   variant: string;
+  full?: boolean;
 };
 
-const Button = ({ type, title, icon, variant }: ButtonProps) => {
+const Button = ({ type, title, icon, variant, full }: ButtonProps) => {
   return (
     <button
-      className={`flexCenter gap-3 rounded-full border ${variant}`}
+      className={`flexCenter ${full && "w-full"} gap-3 rounded-full border ${variant}`}
       type={type}
     >
       {icon && <Image src={icon} alt={title} width={24} height={10} />}
-      <label className="bold-16 whitespace-nowrap">{title}</label>
+      <label className="bold-16 cursor-pointer  whitespace-nowrap">
+        {title}
+      </label>
     </button>
   );
 };
--- components/GetApp.tsx
+++ components/GetApp.tsx
@@ -1,7 +1,39 @@
 import React from "react";
+import Button from "./Button";
+import Image from "next/image";
 
 const GetApp = () => {
-  return <div>GetApp</div>;
+  return (
+    <section className="flex items-center   pb-[100px] justify-center border border-blue-600 ">
+      <div className="mx-auto max-w-[1440px] border  relative flex w-full  flex-col justify-between gap-32 overflow-hidden bg-green-90 bg-pattern bg-cover bg-center bg-no-repeat px-6 py-12 text-white sm:flex-row sm:gap-12 sm:py-24 lg:px-20 xl:max-h-[598px] 2xl:rounded-5xl">
+        <div className="z-20 border border-white flex w-full flex-1 flex-col items-start justify-center gap-12 ">
+          <h2 className="bold-40 lg:bold-64 xl:max-w-[321px]">
+            Get for free new!
+          </h2>
+          <p className="text-gray-10">Available on IOS and Android</p>
+          <div className="flex w-full flex-col gap-3 xl:flex-row ">
+            <Button
+              type="button"
+              title="App Store"
+              icon="/apple.svg"
+              variant="btn_white"
+              full
+            />
+            <Button
+              type="button"
+              title="Play Store"
+              icon="/android.svg"
+              variant="btn_dark_green"
+              full
+            />
+          </div>
+        </div>
+        <div>
+          <Image src="/phones.png" alt="phones" width={550} height={870} />
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default GetApp;
